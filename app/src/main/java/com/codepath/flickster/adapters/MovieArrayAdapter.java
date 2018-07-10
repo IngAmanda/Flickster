9c0ef85acf4afc6a680f37f9b5672d0fafae85d3
Your Name <you@example.com>
2018-07-10 16:43:07 -0400

    Lanscape succes commit

--- app/src/main/java/com/codepath/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/flickster/adapters/MovieArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.flickster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -30,6 +31,7 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie>{
         TextView tvOverview;
         ImageView ivImage;
     }
+    //private Context mContext = null;
 
     @NonNull
     @Override
@@ -70,7 +72,16 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie>{
         viewHolder.tvTitle.setText(movie.getOriginalTitle());
         viewHolder.tvOverview.setText(movie.getOverview());
 
-        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImage);
+
+        int orientation = getContext().getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImage);
+            // ...
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.ivImage);
+            // ...
+        }
+
         // return the view
         return convertView;
     }
--- app/src/main/java/com/codepath/flickster/models/Movie.java
+++ app/src/main/java/com/codepath/flickster/models/Movie.java
@@ -12,12 +12,18 @@ import java.util.ArrayList;
 
 public class Movie {
     String posterPath;
+    String backdropPath;
     String originalTitle;
     String overview;
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
 
     public String getOriginalTitle() {
         return originalTitle;
@@ -31,6 +37,7 @@ public class Movie {
 
     public Movie(JSONObject jsonObject) throws JSONException{
         this.posterPath = jsonObject.getString("poster_path");
+        this.backdropPath = jsonObject.getString("backdrop_path");
         this.originalTitle = jsonObject.getString("original_title");
         this.overview = jsonObject.getString("overview");
     }
